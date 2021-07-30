2a181992f5c36af7361dc1b6dba68e1503acc05b
DeVrtl <88022703+DeVrtl@users.noreply.github.com>
2021-07-30 15:06:43 +0500

    Update EnemySpawner.cs

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,30 +9,30 @@ public class EnemySpawner : MonoBehaviour
 
     
     private int _randomPosition;
-    private float _staticTimeBetweenSpawns;
+    private float _timeBetweenSpawnsNewEnemyTemplate;
     private float _startTimeBetweenSpawns = 2f;
 
     private void Start()
     {
-        _staticTimeBetweenSpawns = _startTimeBetweenSpawns;
+        _timeBetweenSpawnsNewEnemyTemplate = _startTimeBetweenSpawns;
     }
 
     private void Update()
     {
         int random;
 
-        if (_staticTimeBetweenSpawns <= 0)
+        if (_timeBetweenSpawnsNewEnemyTemplate <= 0)
         {
             random = Random.Range(0, _enemyTemplates.Length);
             _randomPosition = Random.Range(0, _spawnPoints.Length);
 
             Instantiate(_enemyTemplates[random], _spawnPoints[_randomPosition].transform.position, Quaternion.identity);
 
-            _staticTimeBetweenSpawns = _startTimeBetweenSpawns;
+            _timeBetweenSpawnsNewEnemyTemplate = _startTimeBetweenSpawns;
         }
         else
         {
-            _staticTimeBetweenSpawns -= Time.deltaTime;
+            _timeBetweenSpawnsNewEnemyTemplate -= Time.deltaTime;
         }
     }
 }
